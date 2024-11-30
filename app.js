b2780d710f3496523c61a7f329b6e1074081cd5c
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-30 15:02:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
@@ -13,28 +12,37 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
+// CORS configuration
 app.use(
   cors({
-    origin: ["https://client-two-iota-81.vercel.app"],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
+    origin: ["https://client-two-iota-81.vercel.app"], // Allowing frontend domain
+    methods: ["GET", "POST", "DELETE", "PUT"], // Allowed methods
+    credentials: true, // Allowing credentials (cookies)
   })
 );
 
+// Middleware setup
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// File upload configuration
 app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
   })
 );
+
+// API Routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
+// Connect to database
 dbConnection();
 
+// Error handling middleware
 app.use(errorMiddleware);
+
 export default app;
