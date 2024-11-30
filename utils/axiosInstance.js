5035722ce05faa5593c80cc417cf4c78156e1d13
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-30 16:24:57 +0530

    Create axiosInstance.js

---
+++ utils/axiosInstance.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://jobbackend-1-prpp.onrender.com",
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
