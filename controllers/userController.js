2b2a374e89ee28351179ec3b51da777abd9b27e7
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-30 15:10:58 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,16 +80,16 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
 });
 
 // Get Logged-in User Details
-export const getUser = catchAsyncErrors((req, res, next) => {
-  const user = req.user;
+// export const getUser = catchAsyncErrors((req, res, next) => {
+//   const user = req.user;
 
-  // Ensure user exists in the request object
-  if (!user) {
-    return next(new ErrorHandler("User not found", 404));
-  }
+//   // Ensure user exists in the request object
+//   if (!user) {
+//     return next(new ErrorHandler("User not found", 404));
+//   }
 
-  res.status(200).json({
-    success: true,
-    user,
-  });
-});
+//   res.status(200).json({
+//     success: true,
+//     user,
+//   });
+// });
