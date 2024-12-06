4897b116d7fcafc627d4c8193795ab9b63372c7f
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-12-06 19:04:57 +0530

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,36 +1,34 @@
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
 
-class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-    }
+  // Handling Mongoose CastError
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400); // Update the error object
   }
-  
-  export const errorMiddleware = (err, req, res, next) => {
-    err.message = err.message || "Internal Server Error";
-    err.statusCode = err.statusCode || 500;
-  
-    if (err.name === "CastError") {
-      const message = `Resource not found. Invalid ${err.path}`,
-        err = new ErrorHandler(message, 400);
-    }
-    if (err.code === 11000) {
-      const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-        err = new ErrorHandler(message, 400);
-    }
-    if (err.name === "JsonWebTokenError") {
-      const message = `Json Web Token is invalid, Try again!`;
-      err = new ErrorHandler(message, 400);
-    }
-    if (err.name === "TokenExpiredError") {
-      const message = `Json Web Token is expired, Try again!`;
-      err = new ErrorHandler(message, 400);
-    }
-    return res.status(err.statusCode).json({
-      success: false,
-      message: err.message,
-    });
-  };
-  
-  export default ErrorHandler;
-  
+
+  // Handling Mongoose Duplicate Key Error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400); // Update the error object
+  }
+
+  // Handling JWT Error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again!`;
+    err = new ErrorHandler(message, 400); // Update the error object
+  }
+
+  // Handling JWT Expire Error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is expired, Try again!`;
+    err = new ErrorHandler(message, 400); // Update the error object
+  }
+
+  // Send the response
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
